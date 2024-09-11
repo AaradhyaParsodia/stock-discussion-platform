6dd8c02d5cc1a1ea0f2fd856cdf5fbcd63a83b44
AaradhyaParsodia <aaradhyaparsodia@gmail.com>
2024-09-11 13:33:46 +0530

    added middleware in like post file

--- src/routes/likePost.js
+++ src/routes/likePost.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { likePost } from "../controllers/index.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 export const likePostRouter = Router();
+
+likePostRouter.use(authMiddleware);
 
 likePostRouter.post("/", likePost.like);
 likePostRouter.delete("/", likePost.unlike)
